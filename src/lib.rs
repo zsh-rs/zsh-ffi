7e00c3ab939d6e4720701853a3c9d82e7b554d79
Cole McAnelly <cole.mcanelly@valentpartners.com>
2026-04-20 13:31:34 -0500

    feat: add compile time tests for different binding groups

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,55 @@
 #![allow(unnecessary_transmutes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub mod zle {}
+
+pub mod modules {}
+
+pub mod builtins {}
+
+
+
+#[cfg(test)]
+mod binding_smoke_tests {
+    #![allow(unused_imports)]
+
+    // Base bindings
+    use super::hashtable;
+    use super::param;
+    use super::builtin;
+    use super::linkroot;
+    use super::shfunc;
+    use super::HashTable;
+    use super::Param;
+    use super::Module;
+    use super::LinkList;
+    use super::addbuiltins;
+    use super::addhookfunc;
+    use super::builtintab;
+
+    // zle::
+    use super::widget;
+    use super::thingy;
+    use super::keymap;
+    use super::Widget;
+    use super::Thingy;
+    use super::Keymap;
+    use super::zrefresh;
+    use super::addzlefunction;
+    use super::zleline;
+    use super::zlecs;
+    use super::setup_zshQszleparameter;
+
+    // modules::
+    use super::setup_zshQsdatetime;
+    use super::setup_zshQslanginfo;
+    use super::setup_zshQsparameter;
+    use super::setup_zshQstermcap;
+    use super::setup_zshQsterminfo;
+    use super::setup_zshQszutil;
+
+    // builtins::
+    use super::setup_zshQssched;
+    use super::setup_zshQsrlimits;
+}
